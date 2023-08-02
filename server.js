6f3119ea89647aa84fe94298208949cc7abd68e1
chesterchan <chesterchan@beNovelty.com>
2023-08-02 21:58:07 +0800

    env

--- server.js
+++
@@ -1,2 +0,0 @@
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,9 @@ export default defineConfig({
             }
         }
     }
-}
+},
+server: {
+    port: 3006,
+  }
   
 })
