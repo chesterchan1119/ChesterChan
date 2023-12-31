1f93c2ead504af36a7e88f8e20edbd993932d343
chesterchan <chesterchan@beNovelty.com>
2023-12-31 17:07:20 +0800

    try mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,77 +1,105 @@
-import React, {Suspense, useEffect, useState } from 'react';
-import {Canvas} from '@react-three/fiber'  ; 
-import {OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-import CanvasLoader from '../Loader'; 
+import CanvasLoader from '../Loader';
 
-
-const Computers = ({isMobile}) => {
+const DesktopComputers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
-  
+
   return (
-    <mesh> 
-        <hemisphereLight intensity={0.15} groundColor ="black" />
-        <pointLight intensity={1}/> 
-        <spotLight 
-          position={[-20,50,10]} 
-          angle={0.12}
-          penumbra={1} 
-          intensity={1} 
-          castShadow
-          shadow-mapSize={1024}
-        
-        /> 
-          
-        <primitive 
-          object={computer.scene}
-          scale = {isMobile? 0.6: 0.6}
-          position = {isMobile? [0, -2, -1.2 ] : [0, -3.25, -1.5 ]}
-          rotation={[-0.001, -0.2,-0.1]}
-        /> 
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+
+      <primitive
+        object={computer.scene}
+        scale={0.6}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.001, -0.2, -0.1]}
+      />
     </mesh>
-  )
-}
+  );
+};
+
+const MobileComputers = () => {
+  const computer = useGLTF('./plain_box/plainBox.gltf');
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+
+      <primitive
+        object={computer.scene}
+        scale={0.6}
+        position={[0, -2, -1.2]}
+        rotation={[-0.001, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false) ;
+  const [isMobile, setIsMobile] = useState(false);
+
   useEffect(() => {
-      //add a listener for changes to the screen size 
-      const mediaQuery = window.matchMedia('(max-width: 500px)');  //check whether we are on a mobile device 
-      
-      // set the initial value of the 'isMobile' state
-      setIsMobile(mediaQuery.matches) ; 
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
 
-     // Define a callback function to handle changes to the media query
-      const handleMediaQueryChange = (event) => { 
-        setIsMobile(event.matches); 
-      }
+    // Set the initial value of the 'isMobile' state
+    setIsMobile(mediaQuery.matches);
 
-      // add the callback function as a listener for changes to the media query 
-      mediaQuery.addEventListener('change' , handleMediaQueryChange);
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
 
-      return () => {
-        mediaQuery.removeEventListener('change' , handleMediaQueryChange); 
-      }
-    }, [])
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
-      frameLoop = "demand"
+      frameLoop="demand"
       shadows
-      camera={{position: [20,3,5] , fov: 25}}
-      gl={{preserveDrawingBuffer: true}}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
-        <Suspense fallback={<CanvasLoader/>}>
-          <OrbitControls 
-          enableZoom={false }
-          maxPolarAngle={Math.PI / 2 }
-          minPolarAngle={Math.PI / 2 }
-          /> 
-          <Computers isMobile={isMobile} /> 
-         
-        </Suspense>
-    
-        <Preload all /> 
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+        {isMobile ? (
+          <Suspense fallback={<CanvasLoader />}>
+            <MobileComputers />
+          </Suspense>
+        ) : (
+          <DesktopComputers />
+        )}
+      </Suspense>
+
+      <Preload all />
     </Canvas>
-  )
-}
- 
-export default ComputersCanvas; 
+  );
+};
+
+export default ComputersCanvas;
